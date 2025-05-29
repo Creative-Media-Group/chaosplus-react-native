9fab0c0bca525e6e389dbb58bd605f8d8374bc55
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-29 15:30:30 +0200

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,81 @@
+import { ScrollView, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ScrollView id="scrollEvents" style={{ width: "100%" }} >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
---
+++ app/videoview.tsx
@@ -0,0 +1,31 @@
+import { useEvent } from "expo";
+import { VideoView, useVideoPlayer } from "expo-video";
+import { StyleSheet } from "react-native";
+const videoSource = 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+
+export default function MyVideoView() {
+    const player = useVideoPlayer(videoSource, player => { player.play(); });
+    const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
+
+    return (
+        <VideoView style={styles.video} player={player} allowsFullscreen allowsPictureInPicture />
+
+    );
+}
+
+const styles = StyleSheet.create({
+    contentContainer: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 50,
+    },
+    video: {
+        width: "100%",
+        height: "100%",
+    },
+    controlsContainer: {
+        padding: 10,
+    },
+});
