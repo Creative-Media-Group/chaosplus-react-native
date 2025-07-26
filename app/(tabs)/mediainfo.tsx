195e862e5228f1166cd2bc9e0bd3ae0b3fbb5b10
tct123 <42028373+tct123@users.noreply.github.com>
2025-07-26 06:41:15 +0200

    .

---
+++ app/(tabs)/mediainfo.tsx
@@ -0,0 +1,5 @@
+import { Image, SafeAreaView, ScrollView, Text } from "react-native";
+
+export default function MediaInfoPage() {
+    return <SafeAreaView><ScrollView><Text>Hello</Text></ScrollView><Image></Image></SafeAreaView>
+}
--- app/mediainfo.tsx
+++
@@ -1,5 +0,0 @@
-import { Image, SafeAreaView, ScrollView } from "react-native";
-
-export function MediaInfoPage() {
-    return <SafeAreaView><ScrollView></ScrollView><Image></Image></SafeAreaView>
-}
