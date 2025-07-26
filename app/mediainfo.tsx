abe01c52ee14bec1ecb2d5f7ed2c11a927d01d96
tct123 <42028373+tct123@users.noreply.github.com>
2025-07-26 05:43:08 +0200

    Create mediainfo.tsx

---
+++ app/mediainfo.tsx
@@ -0,0 +1,5 @@
+import { Image, SafeAreaView, ScrollView } from "react-native";
+
+export function MediaInfoPage() {
+    return <SafeAreaView><ScrollView></ScrollView><Image></Image></SafeAreaView>
+}
