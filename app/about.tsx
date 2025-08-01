d593f56031bb0b7b6d7e6c607b0a512f769aa7ba
tct123 <42028373+tct123@users.noreply.github.com>
2025-08-01 21:31:32 +0200

    .

--- app/about.tsx
+++ app/about.tsx
@@ -1,5 +1,13 @@
-import { Image, SafeAreaView, ScrollView, Text } from "react-native";
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text } from "react-native";
 
 export default function MediaInfoPage() {
-    return <SafeAreaView><ScrollView><Text>Hello</Text></ScrollView><Image></Image></SafeAreaView>
+    return <SafeAreaView ><ScrollView style={style.infopage}><Text>Hello</Text></ScrollView><Image></Image></SafeAreaView>
 }
+const style = StyleSheet.create({
+    infopage: {
+        alignContent: "center", // alignItems: "center",
+        alignSelf: "center",
+        width: "100%",
+        height: "100%"
+    }
+})
