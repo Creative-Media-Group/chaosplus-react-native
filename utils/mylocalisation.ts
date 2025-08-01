15f2ad68e80ddae2eb2d799edb04fc50453d45ef
tct123 <42028373+tct123@users.noreply.github.com>
2025-08-01 20:57:16 +0200

    .

---
+++ utils/mylocalisation.ts
@@ -0,0 +1,14 @@
+import { getLocales } from "expo-localization";
+import { I18n } from "i18n-js";
+import de from "../localisation/translations/de.json";
+import en from "../localisation/translations/en.json";
+
+export const deviceLanguage = getLocales()?.[0]?.languageCode ?? "en";
+
+export const i18n = new I18n({
+    en,
+    de,
+});
+
+i18n.defaultLocale = deviceLanguage;
+i18n.locale = deviceLanguage;
