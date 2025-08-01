6a7698d0b40de70bbfa6787c66d07b1c48908e4b
tct123 <42028373+tct123@users.noreply.github.com>
2025-08-01 21:44:48 +0200

    Update about.tsx

--- app/about.tsx
+++ app/about.tsx
@@ -1,13 +1,21 @@
+import { Link } from "expo-router";
 import { Image, SafeAreaView, ScrollView, StyleSheet, Text } from "react-native";
 
 export default function MediaInfoPage() {
-    return <SafeAreaView ><ScrollView style={style.infopage}><Text>Hello</Text></ScrollView><Image></Image></SafeAreaView>
+    return <SafeAreaView >
+        <ScrollView style={style.infopage}>
+            <Text>Repository: </Text>
+            <Link href={"https://google.com"} style={style.link}>Hello</Link>
+        </ScrollView>
+        <Image>
+        </Image>
+    </SafeAreaView>
 }
 const style = StyleSheet.create({
     infopage: {
         alignContent: "center", // alignItems: "center",
         alignSelf: "center",
         width: "100%",
-        height: "100%"
-    }
+        height: "100%",
+    }, link: { color: "blue", fontSize: 20 }
 })
