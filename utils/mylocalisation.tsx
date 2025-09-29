08c3d3755496eab9b5186435e9797b70d812da63
tct123 <42028373+tct123@users.noreply.github.com>
2025-09-29 16:53:06 +0200

    .

--- utils/mylocalisation.tsx
+++ utils/mylocalisation.tsx
@@ -3,12 +3,13 @@ import { I18n } from "i18n-js";
 import de from "../localisation/translations/de.json";
 import en from "../localisation/translations/en.json";
 
-export const deviceLanguage = getLocales()?.[0]?.languageCode ?? "en";
+export const deviceLanguage = String(getLocales()?.[0]?.languageCode);
 
 export const i18n = new I18n({
     en,
     de,
 });
 
-i18n.defaultLocale = deviceLanguage;
+i18n.enableFallback = true;
+i18n.defaultLocale = "en";
 i18n.locale = deviceLanguage;
