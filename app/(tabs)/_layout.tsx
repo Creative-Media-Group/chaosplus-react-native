a26d2b730751bc9a8ffe5a4aa8642aaf8706a33c
tct123 <42028373+tct123@users.noreply.github.com>
2025-09-29 17:38:10 +0200

    .

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,35 @@
-import { Stack } from "expo-router";
+import { i18n } from "@/utils/mylocalisation";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
 export default function RootLayout() {
-  return <Stack />;
+  let hometitle = i18n.t("home");
+  let abouttitle = i18n.t("about");
+  return <Tabs><Tabs.Screen
+    name="index"
+    options={{
+      title: hometitle,
+      tabBarIcon: ({ color, size }) => (
+        <Ionicons name="home" color={color} size={size} />
+      ),
+    }}
+  />
+    <Tabs.Screen
+      name="about"
+      options={{
+        title: abouttitle,
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="information" color={color} size={size} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="(tabs)"
+      options={{
+        title: abouttitle,
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="information" color={color} size={size} />
+        ),
+      }}
+    /></Tabs>;
 }
