bb6530b41bbc75331fed7e6ad02f6b7c87320df6
tct123 <42028373+tct123@users.noreply.github.com>
2025-09-29 05:51:29 +0200

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,8 +3,8 @@ import { Image, ImageSourcePropType, Text, TouchableOpacity } from "react-native
 export default function Card(img_src: ImageSourcePropType, text: string) {
     return (
         <TouchableOpacity>
-            <Image source={img_src}></Image>
-            <Text>${text}</Text>
+            <Image source={img_src} />
+            <Text >${text}</Text>
         </TouchableOpacity>
     )
 }
