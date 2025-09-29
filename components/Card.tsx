e10d01f56d7d3031e689df1381ebff8a35502a57
tct123 <42028373+tct123@users.noreply.github.com>
2025-09-29 06:15:13 +0200

    .

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,10 +1,21 @@
 import { Image, ImageSourcePropType, Text, TouchableOpacity } from "react-native";
 
-export default function Card(img_src: ImageSourcePropType, text: string) {
+interface CardProbs {
+    onPress: () => void;
+    img_src: ImageSourcePropType,
+    text: string
+
+}
+export default function Card({ img_src, text }: CardProbs) {
     return (
-        <TouchableOpacity>
+        <TouchableOpacity style={{
+            width: 16 * 15,
+            height: 9 * 15,
+            backgroundColor: "red",
+            borderRadius: 20
+        }}>
             <Image source={img_src} />
-            <Text>${text}</Text>
+            <Text>{text}</Text>
         </TouchableOpacity>
     )
 }
