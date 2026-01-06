9604a664b2b20b5ee108ca72a78696a8fa199aa8
tct123 <42028373+tct123@users.noreply.github.com>
2026-01-06 19:37:23 +0100

    Update mediainfo.tsx

--- app/(tabs)/mediainfo.tsx
+++ app/(tabs)/mediainfo.tsx
@@ -1,8 +1,7 @@
-import { JSXElementConstructor, ReactElement, ReactNode, ReactPortal } from "react";
 import { Image, ImageSourcePropType, ScrollView, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-function MediaInfoContent(img_src: ImageSourcePropType | undefined, text: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined) {
+function MediaInfoContent(img_src: ImageSourcePropType | undefined, text: string) {
   return <ScrollView>
     <Image source={img_src} />
     <Text>{text}</Text>
