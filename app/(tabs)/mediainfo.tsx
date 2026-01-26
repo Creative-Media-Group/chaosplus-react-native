fc1d296c1591a5f4ac939115b7627e127afed18a
tct123 <42028373+tct123@users.noreply.github.com>
2026-01-26 11:52:44 +0100

    Refactor MediaInfoContent to use props object
    Changed MediaInfoContent to accept a props object with img_src and text properties, and updated its usage in MediaInfoPage to pass the required props.

--- app/(tabs)/mediainfo.tsx
+++ app/(tabs)/mediainfo.tsx
@@ -1,7 +1,11 @@
 import { Image, ImageSourcePropType, ScrollView, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-function MediaInfoContent(img_src: ImageSourcePropType | undefined, text: string) {
+interface MediaInfoContentProbs {
+  img_src: ImageSourcePropType,
+  text: string
+}
+function MediaInfoContent({ img_src, text }: MediaInfoContentProbs) {
   return <ScrollView>
     <Image source={img_src} />
     <Text>{text}</Text>
@@ -11,7 +15,7 @@ function MediaInfoContent(img_src: ImageSourcePropType | undefined, text: string
 export default function MediaInfoPage() {
   return (
     <SafeAreaView>
-      <MediaInfoContent />
+      <MediaInfoContent img_src={{ uri: "asset:assets/I" }} text="hello" />
     </SafeAreaView>
   );
 }
