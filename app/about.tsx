21e725b6c328ea9f49d5f5f2a244ce5421a68b99
tct123 <42028373+tct123@users.noreply.github.com>
2026-06-27 16:58:20 +0200

    .

--- app/about.tsx
+++ app/about.tsx
@@ -1,25 +1,23 @@
 import { Link } from "expo-router";
-import { Image, ScrollView, Text, useColorScheme } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView, Text, useColorScheme } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useStyles } from "../styles/styles";
 
 export default function MediaInfoPage() {
   const colorSheme = useColorScheme();
+  const insets = useSafeAreaInsets();
   const styles = useStyles();
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView style={styles.infopage}>
-        <Text style={styles.infopagetext}>Repository: </Text>
-        <Link
-          href={
-            "https://github.com/Creative-Media-Group/chaosplus-react-native"
-          }
-          style={styles.link}
-        >
-          Chaos+
-        </Link>
-      </ScrollView>
-      <Image></Image>
-    </SafeAreaView>
-  );
+  return <ScrollView style={[styles.infopage, { paddingTop: insets.top }]}>
+    <Text style={styles.infopagetext}>Repository: </Text>
+    <Link
+      href={
+        "https://github.com/Creative-Media-Group/chaosplus-react-native"
+      }
+      style={styles.link}
+    >
+      Chaos+
+    </Link>
+  </ScrollView>
+  //<Image></Image>
+
 }
