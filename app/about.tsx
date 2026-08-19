b5e407c681a692c217f94194e53900e3f6b0ffce
tct123 <42028373+tct123@users.noreply.github.com>
2026-08-19 07:18:42 +0200

    Update about.tsx

--- app/about.tsx
+++ app/about.tsx
@@ -1,10 +1,9 @@
 import { Link } from "expo-router";
-import { ScrollView, Text, useColorScheme } from "react-native";
+import { ScrollView, Text } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useStyles } from "../styles/styles";
 
 export default function MediaInfoPage() {
-  const colorSheme = useColorScheme();
   const insets = useSafeAreaInsets();
   const styles = useStyles();
   return <ScrollView style={[styles.infopage, { paddingTop: insets.top }]}>
